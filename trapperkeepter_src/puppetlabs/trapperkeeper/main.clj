3888e15aeb8f01a8eb6198eb2fb16c964ad8703b
janelu2 <jane@puppet.com>
2016-07-18 09:34:52 -0700

    Changed up example filesO

--- trapperkeepter_src/puppetlabs/trapperkeeper/main.clj
+++
@@ -1,8 +0,0 @@
-(ns puppetlabs.trapperkeeper.main
-  (:gen-class))
-
-(defn -main
-  [& args]
-  (require 'puppetlabs.trapperkeeper.core)
-  (apply (resolve 'puppetlabs.trapperkeeper.core/main) args))
-
